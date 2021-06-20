abdaf93077f25bd8556e3356cdd002936989546f
Paul Brady <pablo.brady@gmail.com>
2021-06-20 13:37:17 -0700

    TailwindUI Toggle WIP

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <img class="mx-auto" width="100" height="100" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="HELLO V3 WORLD!" />
-  <EnableNotifications />
+
+  <div class="m-4 p-3">
+    <EnableNotifications />
+  </div>
+
+  <div class="m-4 p-3">
+    <ToggleNewsletters />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import EnableNotifications from './components/EnableNotifications.vue'
+import ToggleNewsletters from './components/ToggleNewsletters.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
-    EnableNotifications
+    EnableNotifications,
+    ToggleNewsletters
+  },
+  data() {
+    return {
+      users: [
+        {
+          name: "John",
+          age: 35,
+          favorite: false,
+        },
+        {
+          name: "Jane",
+          age: 30,
+          favorite: true,
+        },
+      ]
+    }
   }
 })
 </script>
--- src/components/EnableNotifications.vue
+++ src/components/EnableNotifications.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="py-16">
-    <h2 class="text-xl font-bold text-left px-3">EnableNotifications:</h2>
-    <Switch
-      v-model="enabled"
-      :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
-      class="relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-    >
-      <span class="sr-only">Use setting</span>
-      <span
-        aria-hidden="true"
-        :class="enabled ? 'translate-x-9' : 'translate-x-0'"
-        class="pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"
-      />
-    </Switch>
-  </div>
+  <SwitchGroup>
+    <div class="flex justify-between items-center max-w-sm mx-auto">
+      <SwitchLabel class="mr-4">Enable notifications</SwitchLabel>
+      <Switch
+        v-model="enabled"
+        :class='enabled ? "bg-blue-600" : "bg-gray-200"'
+        class="relative inline-flex items-center h-6 transition-colors rounded-full w-11 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        <span
+          :class='enabled ? "translate-x-6" : "translate-x-1"'
+          class="inline-block w-4 h-4 transition-transform transform bg-white rounded-full"
+        />
+      </Switch>
+    </div>
+  </SwitchGroup>
 </template>
 
-<script lang="ts">
-import { ref } from 'vue'
-import { Switch } from '@headlessui/vue'
+<script>
+  import { ref } from 'vue'
+  import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
 
-export default {
-  components: { Switch },
+  export default {
+    components: { Switch, SwitchGroup, SwitchLabel },
 
-  setup() {
-    const enabled = ref(false)
+    setup() {
+      const enabled = ref(false)
 
-    return { enabled }
-  },
-}
+      return { enabled }
+    },
+  }
 </script>
